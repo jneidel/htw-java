289612c235359783c444890f2eea112997d6ddd7
Jonathan Neidel <jneidel@jneidel.com>
2019-11-01 18:18:56 +0100

    Fix Transformation and UnkownProblems

--- ktechen/KW. 44/Transformation.java
+++ ktechen/KW. 44/Transformation.java
@@ -1,41 +1,29 @@
-
 public class Transformation {
-
 	public static void main(String[] args) {
-
 		int basis = 5;
 		int exponent = 2;
 		int ergebnis = 1;
 		int anzahl;
 		
-		/*
+    /*
 		for (anzahl = 0; anzahl < exponent; ++anzahl) {
 			ergebnis = ergebnis * basis;
 		}
-		*/
+    */
 		
-		
-		
-		//int anzahl = 0;
-		/*
+		anzahl = 0; // anzahl muss außerhalb der Schleife initialisiert werden
 		while(anzahl < exponent) {
-			
 			ergebnis = ergebnis * basis;
-			anzahl++;
-			
+			anzahl++; // anzahl muss innerhalb der Schleife hochgezählt werden
 		}
-		*/
 		
-		/*
-		
-		do {
+		anzahl = 0; // Wieder vor der Schleife initialisiert
+		do { // Wird auch ausgeführt wenn anzahl >= exponent
 			ergebnis = ergebnis * basis;
 			anzahl++;
-		}while(anzahl < exponent);
+		} while (anzahl < exponent);
 		
-		*/
 		System.out.println(basis + " hoch " + exponent + " = " + ergebnis);
 		System.out.println("Anzahl der Durchläufe: " + anzahl);
 	}
-
 }
