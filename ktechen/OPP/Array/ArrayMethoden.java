db668bfd9a250feba76831aaa1abcc3bd874ce69
Ktechen <57128890+Ktechen@users.noreply.github.com>
2019-12-10 18:59:43 +0100

    change ArrayMethoden

--- ktechen/OPP/Array/ArrayMethoden.java
+++ ktechen/OPP/Array/ArrayMethoden.java
@@ -1,8 +1,42 @@
-package source;
+package OPP.Array;
 
 public class ArrayMethoden {
 
-	public static int arrayGet(int[] array, int i) {
+	/*
+	 * Outcome of all function
+	 */
+	public static void printArrayInformation(int[] array,int ArrayAnStelleX)
+	{	
+		final String BORDER = "#==============================================================================================#";
+		
+		System.out.println(BORDER);
+		
+		System.out.printf("| Array der Zahl x            :  %d\n", ArrayMethoden.arrayGet(array, ArrayAnStelleX), "|");
+		System.out.printf("| Summe                       :  %d\n", ArrayMethoden.sum(array));
+		System.out.printf("| Mittelwert                  :  %d\n", ArrayMethoden.mittelwert(array));
+		System.out.printf("| Anzahl der Geradenzahlen    :  %d\n", ArrayMethoden.countGeradeZahl(array));
+		System.out.printf("| Zwei größste Zahl           :  %d\n", ArrayMethoden.zweiGroesssteZahl(array));
+		System.out.printf("| Größste Zahl                :  %d\n", ArrayMethoden.max(array));
+		System.out.printf("| Kleinste Zahl               :  %d\n", ArrayMethoden.min(array));
+		System.out.print("| Alle Arrays in Quadrat      :  ");
+		/*
+		 * VOID
+		 */
+		ArrayMethoden.quadrat(array);
+		
+		System.out.println();
+		System.out.print("| Alle Geraden Arrays         :  ");
+
+		ArrayMethoden.geradeZahl(array);
+	
+		System.out.println();
+	
+		System.out.println(BORDER);
+	}
+	/*
+	 * Search a int from Array
+	 */
+	private static int arrayGet(int[] array, int i) {
 		int num = 0;
 		
 		if(i < Integer.MIN_VALUE) {
@@ -16,7 +50,10 @@ public class ArrayMethoden {
 		return num;
 	}
 	
-	public static int sum (int[] array) {
+	/*
+	 * Addiert das Array zusammen
+	 */
+	private static int sum (int[] array) {
 		int num = 0;
 		
 		for (int i = 0; i < array.length; i++) {
@@ -26,7 +63,10 @@ public class ArrayMethoden {
 		return num;
 	}
 	
-	public static int mittelwert(int[] array) {
+	/*
+	 * Mittelwert
+	 */
+	private static int mittelwert(int[] array) {
 		if(array.length <= 0) {
 			return 0;
 		}
@@ -34,20 +74,82 @@ public class ArrayMethoden {
 		return sum(array)/array.length;
 	}
 	
-	public static int quadrat(int[] array) {
-		int num = 1;
+	/*
+	 * quadrat das Array
+	 */
+	private static void quadrat(int[] array) {
 		
 		for (int i = 0; i < array.length; i++) {
-			num *= array[i];
+			System.out.print(Math.pow(arrayGet(array, i),2) + " ");
 		}
 		
-		return num;
 	}
 	
-	public static void println(int x) {
-
+	/*
+	 * Größste Zahl
+	 */
+	private static int max(int[] array) {
+		int max = Integer.MIN_VALUE;
 		
-		System.out.println(x);
+		for (int i = 0; i < array.length; i++) {	
+			if(max < array[i]) {
+				max = arrayGet(array, i);
+			}
+		}
+		
+		return max;
+	}
+	
+	/*
+	 * kleinste Zahl
+	 */
+	private static int min(int[] array) {
+		int min = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < array.length; i++) {	
+			if(min > array[i]) {
+				min = arrayGet(array, i); 
+			}
+		}
+		return min;
+	}
+	
+	/*
+	 * Zahlen des Array sind Gerade
+	 */
+	private static void geradeZahl(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if(array[i]%2 == 0) {
+				System.out.print(arrayGet(array, i) + " ");	
+			}
+		}
+	}
+
+	/*
+	 * Anzahl der Geradenzahlen
+	 */
+	private static int countGeradeZahl(int[] array) {
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			if(array[i]%2 == 0) {
+				count++;	
+			}
+		}
+		
+		return count;
+	}
+	
+	private static int zweiGroesssteZahl(int[] array) {
+		int max = max(array);
+		int placetwo = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			if(max > array[i]) {
+				placetwo = array[i];
+			}
+		}
+		
+		return placetwo;
 	}
 	
 }
--- ktechen/OPP/Array/TestArray.java
+++ ktechen/OPP/Array/TestArray.java
@@ -1,20 +1,14 @@
-package source;
+package OPP.Array;
 
 public class TestArray {
 
 	public static void main(String[] args) {
 		ArrayMethoden arrayM = new ArrayMethoden();
 
-		
-		int[] myNum = {10,20,30,40}; 
-		ArrayMethoden.println(ArrayMethoden.arrayGet(myNum, 2));
-		
-		ArrayMethoden.println(ArrayMethoden.sum(myNum));
-		
-		ArrayMethoden.println(ArrayMethoden.mittelwert(myNum));
-		
-		ArrayMethoden.println(ArrayMethoden.quadrat(myNum));
-		
+		int[] array = {10,20,30,40,41}; 
+		int[] array2 = {1,2,3,4,5,6,7,11,17};
+		ArrayMethoden.printArrayInformation(array, 4);
+		ArrayMethoden.printArrayInformation(array2, 7);
 	}
 
 }
