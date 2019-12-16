1f381285786a32c2e7015d1870b85299f0397e1f
Ktechen <57128890+Ktechen@users.noreply.github.com>
2019-12-16 08:48:37 +0100

    add "squareCalulationOfMatrix(double[][] matrix)"

--- ktechen/OPP/MatrixSpur/MatrixUtils.java
+++ ktechen/OPP/MatrixSpur/MatrixUtils.java
@@ -36,7 +36,7 @@ public class MatrixUtils {
 		
 		int row = 0; //zeile
 		int column = 0; //spalte
-		double result = 0; 
+		double result = 0;
 		
 		for (int i = 0; i < matrix.length; i++) {
 			row = i + 1;
@@ -50,18 +50,10 @@ public class MatrixUtils {
 		//System.out.println(row + " " + column);
 		
 		//if(row == column) { System.out.println("Es wurde ein quadratisches Array übergeben.");  } /*quadratisch */ 
-		if(row != column) { 
-			System.out.println("Es wurde kein quadratisches Array übergeben."); 
-		} /*rechtecking*/ 
-		else{
-			
-			for (int i = 0; i < matrix.length; i++) {
-				for (int j = 0; j < matrix[i].length; j++) {
-					if(i == j) {
-						result += matrix[i][j];
-					}
-				}
-			}
+		if(row != column) { System.out.println("Es wurde kein quadratisches Array übergeben."); } /*rechtecking*/ 
+		else{ /*Quadratisch*/
+
+			result = squareCalulationOfMatrix(matrix);
 			//System.out.println("Test: " + result);
 			
 			/*
@@ -74,5 +66,20 @@ public class MatrixUtils {
 		}
 		return result;
 	}
+	
+	private static double squareCalulationOfMatrix(double[][] matrix) {
+		double result = 0;
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if(i == j) {
+					result += matrix[i][j];
+				}
+			}
+		}
+		
+		return result;
+	}
+	
 
 }
