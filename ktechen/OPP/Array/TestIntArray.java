45010805c44685ad8cc7ba2af0391b88add430f7
Ktechen <57128890+Ktechen@users.noreply.github.com>
2020-01-09 12:35:52 +0100

    Execise 11

---
+++ ktechen/OPP/Array/TestIntArray.java
@@ -0,0 +1,59 @@
+package OPP.Array;
+
+public class TestIntArray {
+
+	public static void main(String[] args) {
+		int[] array = {-1,3,6,7,19};
+		int[] array1 = null;
+		int[] array2 = {1, 0, 1, 976, -1, 12, 0};
+		
+		IntArray intArray = new IntArray();
+		
+		int[][] arrays = {array, array1};
+		
+		/*
+		 * part 1
+		 */
+		for (int[] i : arrays) {
+			intArray.setArray(i);
+			intArray.gibtAus();
+		}
+		
+		/*
+		 * part 2
+		 */
+		for (int[] i : arrays) {
+			intArray.suche(i, 6);
+		}
+		
+		/*
+		 * part 3
+		 */
+		intArray.setArray(array2);
+		intArray.gibtAus();
+		intArray.fuegeEin(5);
+		intArray.gibtAus();
+		intArray.fuegeEin(30);
+		intArray.gibtAus();
+		intArray.fuegeEin(50);
+		intArray.gibtAus();
+		
+		/*
+		 * part 4
+		 * 
+		 */
+		
+		int[] arrayA = {-1,3,6,7,19};
+		int[] arrayB = {20,30,40,50,60};
+
+		try {
+			System.out.println();
+			System.out.println(IntArray.verketter(arrayA, arrayB));
+			
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
